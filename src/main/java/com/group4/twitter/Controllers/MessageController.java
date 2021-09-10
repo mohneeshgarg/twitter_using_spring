6f615d23fcdd108763ab83b4f48d8126c654e1ce
Mohammad Shakir in <mohammad.shakir.in@jasperindia.local>
2021-09-10 13:18:59 +0530

    messages restAPI

--- src/main/java/com/group4/twitter/Controllers/MessageController.java
+++ src/main/java/com/group4/twitter/Controllers/MessageController.java
@@ -1,71 +1,43 @@
 package com.group4.twitter.Controllers;
 
-import com.group4.twitter.DAO.UserDAO;
 import com.group4.twitter.Entities.Message;
-import com.group4.twitter.Entities.User;
 import com.group4.twitter.Services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.util.List;
 
-@Controller
+@RestController
 public class MessageController {
-    @Bean
-    public RestTemplate getRestTemplate(){
-        return new RestTemplate();
-    }
-
-    @Autowired
-    UserDAO userDAO;
-    @Autowired
-    RestTemplate restTemplate;
-
     @Autowired
     MessageService messageService;
-    @RequestMapping("/messages")
-    public ModelAndView openMessagePage(){
-        ModelAndView mv = new ModelAndView("messages");
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String username = ((UserDetails)auth.getPrincipal()).getUsername();
-        User user = userDAO.findByUserName(username);
-        List<Message> sentMessages = messageService.findSentMessages(user.getId());
-        List<Message> receivedMessages = messageService.findReceivedMessages(user.getId());
-        mv.addObject("id", user.getId());
-        mv.addObject("sent", sentMessages);
-        mv.addObject("received", receivedMessages);
-        System.out.println(sentMessages);
-        System.out.println(receivedMessages);
-        return mv;
+
+    @GetMapping("/test")
+    public String test(){
+        return "Its working.";
     }
-    @GetMapping("/user/{id}/messages/new")
-    public ModelAndView newMessage(@PathVariable int id){
-        ModelAndView mv = new ModelAndView("newMessage");
-        mv.addObject("id", id);
-        return mv;
+
+    @GetMapping("/user/{id}/message/sent")
+    public List<Message> getSentMessages(@PathVariable("id") int senderId){
+        List<Message> sentList = messageService.findSentMessages(senderId);
+        System.out.println(sentList);
+        return sentList;
     }
-    @GetMapping("/messages/{id}/new")
-    public String addMessage(@PathVariable int id,
-                             @RequestParam("messageBody") String messageBody,
-                             @RequestParam("receiver") String receiver){
-        User toUser = userDAO.findByUserName(receiver);
-        User fromUser = userDAO.findById(id).get();
+
+    @GetMapping("/user/{id}/message/received")
+    public List<Message> getReceivedMessages(@PathVariable("id") int receiverId){
+        List<Message> receivedList =  messageService.findReceivedMessages(receiverId);
+        System.out.println(receivedList);
+        return receivedList;
+    }
+
+    @PostMapping("/message/new")
+    public String newMessage(@RequestBody Message message){
         Date current_date = new Date();
-        Message message = new Message(messageBody, id, toUser.getId(), fromUser.getName(), toUser.getName(), current_date, current_date);
-        System.out.println(toUser.getName());
+        message.setDate(current_date);
+        message.setTime(current_date);
         messageService.insert(message);
-        System.out.println(message);
-        System.out.println("Message Created!");
-        return "redirect:/";
+        return "Message Created";
     }
 }
--- src/main/java/com/group4/twitter/DAO/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.group4.twitter.DAO;
-
-import com.group4.twitter.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends JpaRepository<User, Integer> {
-    User findByUserName(String username);
-}
